a0f051905335776effeb1dbd2fc4771de44ccd47
=Nandini Swaraj <=>
2020-03-27 21:10:33 +0530

    [Nandiniswaraj]Add.MoodAnalyserUC1

---
+++ main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.moodanalyser;
+
+public class MoodAnalyser {
+    private String message;
+
+    public MoodAnalyser() {
+    }
+
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+
+    public String analyseMood()  {
+            if (message.contains("sad"))
+                return "SAD";
+            return "HAPPY";
+        }
+
+
+
+}
