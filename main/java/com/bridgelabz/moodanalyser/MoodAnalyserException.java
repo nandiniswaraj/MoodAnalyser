107d29684a9815c5e0fd113c708731062efd783f
=Nandini Swaraj <=>
2020-03-27 21:46:48 +0530

    [Nandiniswaraj]Add.MoodException

--- main/java/com/bridgelabz/moodanalyser/MoodAnalyserException.java
+++ main/java/com/bridgelabz/moodanalyser/MoodAnalyserException.java
@@ -1,16 +1,17 @@
 package com.bridgelabz.moodanalyser;
 
-public class MoodAnalyserException extends  Exception{
-    public  enum  exceptionType { ENTERED_NULL,ENTERED_EMPTY};
+public class MoodAnalyserException extends Throwable
+{
+public enum exceptionType { ENTERED_NULL, ENTERED_EMPTY, NO_SUCH_CLASS, NO_SUCH_METHOD, METHOD_INVOCATION_ISSUE,
+    NO_ACCESS, OBJECT_CREATION_ISSUE, NO_SUCH_FIELD,FIELD_SETTING_ISSUE};
     exceptionType type;
-    public MoodAnalyserException(String message)
-    {
-        super(message);
-    }
-    public MoodAnalyserException(exceptionType type, String message)
-    {
-        super(message);
-        this.type=type;
-    }
 
-}
+public MoodAnalyserException(String message) {
+        super(message);
+        }
+
+public MoodAnalyserException( exceptionType type, String message) {
+        super(message);
+        this.type = type;
+        }
+        }
