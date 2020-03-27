c1df6c784907f493137b5179e65340d557e6aedb
=Nandini Swaraj <=>
2020-03-27 21:11:42 +0530

    [Nandiniswaraj]Add.MoodAnalyserTestUc1

---
+++ test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.moodanalyser;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+import static org.junit.Assert.*;
+
+public class MoodAnalyserTest {
+
+    @Test
+    public void whenMood_IsSad_ShouldReturnSad() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad Mood");
+           String mood = moodAnalyser.analyseMood();
+
+        Assert.assertEquals("SAD", mood);
+    }
+
+    @Test
+    public void whenIn_AnyMood_ShouldReturnHappy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in happy Mood");
+          String  mood = moodAnalyser.analyseMood();
+        Assert.assertEquals("HAPPY", mood);
+    }
+}
