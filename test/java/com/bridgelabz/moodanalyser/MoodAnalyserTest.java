26392a98e3fdde32e23956c26dafdec30950f361
nandiniswaraj <61447397+nandiniswaraj@users.noreply.github.com>
2020-04-28 02:54:28 -0400

    Update MoodAnalyserTest.java

--- test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
+++ test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -11,14 +11,12 @@ public class MoodAnalyserTest {
     public void whenMood_IsSad_ShouldReturnSad() {
         MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad Mood");
            String mood = moodAnalyser.analyseMood();
-
-        Assert.assertEquals("SAD", mood);
+           Assert.assertEquals("SAD", mood);
     }
-
     @Test
     public void whenIn_AnyMood_ShouldReturnHappy() {
         MoodAnalyser moodAnalyser = new MoodAnalyser("I am in happy Mood");
           String  mood = moodAnalyser.analyseMood();
-        Assert.assertEquals("HAPPY", mood);
+         Assert.assertEquals("HAPPY", mood);
     }
 }
